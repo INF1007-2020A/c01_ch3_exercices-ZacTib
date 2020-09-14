a70a29ce9fcfbae91f9c2c6a3388dfd61e9c9e8d
ZacTib <zachary.thibodeau@polymtl.ca>
2020-09-14 16:57:10 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -17,7 +17,7 @@ def to_degrees(angle_rads: float) -> tuple:
     angle = angle_rads * 180 / math.pi
     angle_degs = int(angle)
     reste = angle - angle_degs
-    a
+    angle_mins = reste
     reste2 = reste - 
     return int(angle), , 
 
